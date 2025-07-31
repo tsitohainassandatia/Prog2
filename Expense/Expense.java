10c14d72c94c74bb962389ebecf5295cd9c108f7
tsitohainassandatia <hei.sandatia@gmail.com>
2025-07-31 10:57:48 +0100

    creation du constructeur et des getters

--- Expense/Expense.java
+++ Expense/Expense.java
@@ -1,4 +1,29 @@
 package Expense;
+
+import java.time.LocalDate;
+
 public class Expense {
+    private String label;
+    private double amount;
+    private LocalDate date;
+
+    public Expense(String label, double amount, LocalDate date){
+        this.label=label;
+        this.amount=amount;
+        this.date=date;
+    }
+
+    public String getLabel(){
+        return label;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
     
 }
